54e827df2e3ac716531f5a0584f5d79e1aaf46c4
Pauniel <st10493951@rcconnect.edu.za>
2025-10-12 12:31:33 +0200

    Added comments to Main class

--- ProgPOE/src/main/java/Main.java
+++ ProgPOE/src/main/java/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Registration
+        // Calls the reisterUser() method from Login class
         Login user = Login.registerUser(reader);
 
-        // Login
+        // A loop that will keep looping until the correct login details are entered
         boolean loggedIn = false;
         while (!loggedIn) {
             System.out.print("Enter Username: ");
@@ -15,17 +15,18 @@ public class Main {
             System.out.print("Enter Password: ");
             String loginPassword = reader.nextLine();
 
+            //Validates login details using the returnLoginStatus() method
             if (user.returnLoginStatus(loginUsername, loginPassword)) {
-                loggedIn = true;
+                loggedIn = true; //Loop breaks when the login is successfull
             } else {
                 System.out.println("Login failed. Try again.");
             }
         }
 
-        // Welcome Message
+        // Welcome Message is displayed after user is successfully logged in
         System.out.println("\nWelcome to QuickChat.");
 
-        // Start QuickChat menu
+        // Calls the startChat() method from the message class after the welcome message is shown
         Message.startChat(reader);
     }
 }
